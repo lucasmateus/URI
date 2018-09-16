89a7ea1f96bc9045ae9aaa8a2db5f784a0599354
Lucas Mateus <31706781+lukinhasm07@users.noreply.github.com>
2018-09-16 00:41:57 -0300

    Create 1001.java

---
+++ JAVA/1001.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int X;
+    Scanner leitor = new Scanner(System.in);
+    int A = leitor.nextInt();
+    int B = leitor.nextInt();
+    X=A+B;
+    System.out.println("X = "+X);
+  }
+}
