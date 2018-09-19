3cd4b988d335f14fa4cf47f1cebae053470d4788
Lucas Mateus <31706781+lukinhasm07@users.noreply.github.com>
2018-09-19 17:31:33 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,14 +6,16 @@ class Main {
     Arvore arv = new Arvore(1);
     System.out.println("Elemento raiz: "+arv.root().element());
     arv.addChild(arv.root(), 4);
-    arv.addChild(v, o);
+    Iterator<No> segundaGeração = arv.root().children();
+    arv.addChild(segundaGeração.next(), 6);
     Iterator it = arv.elements();
-    Iterator its = arv.Nos();
+    Iterator<No> its = arv.Nos();
     System.out.println("elementos da Arvore:");
     while(it.hasNext())
       System.out.println(it.next());
     System.out.println("elementos da Arvore pelo nó:");
     while(its.hasNext())
-      System.out.println( ((No) its.next()).element());
+      System.out.println(its.next().element());
+    System.out.println("Tamanho da Arvore: "+arv.height(arv.root()));
   }
 }
